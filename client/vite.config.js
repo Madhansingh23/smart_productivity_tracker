c2981484c60c0ead595172ab170d403ddfb00db1
Madhan Singh T <t.madhansingh23@gmail.com>
2025-08-20 01:17:48 +0530

    try

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000", // for local dev
         changeOrigin: true,
       },
     },

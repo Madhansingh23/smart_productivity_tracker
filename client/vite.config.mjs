66205b4c628c282d4680816391c8e39b1d902783
Madhan Singh T <t.madhansingh23@gmail.com>
2025-08-19 15:44:48 +0530

    chcke ing by changing ignore+viteconfig

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,4 +1,13 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], server: { port: 3000 } });
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',   // Vercel expects dist inside client (client/dist)
+    emptyOutDir: true
+  }
+});
